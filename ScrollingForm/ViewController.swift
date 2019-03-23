f519266a94598a7d0237ad5ea31e6838e87ade89
Shalise Ayromloo <shaliseayromloo@shalises-macbook-air.local>
2019-03-22 21:13:56 -0500

    This app is based on lab 4.4 in App Development With Swift Textbook. This app implements scroll views with auto layouts.

--- ScrollingForm/ViewController.swift
+++ ScrollingForm/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        registerForKeyboardNotifications()
     }
 
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: .UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)),name: .UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWasShown(_ notificiation: NSNotification) {
+        guard let info = notificiation.userInfo, let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue else { return }
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        let contentInsets = UIEdgeInsets(0.0, 0.0, keyboardSize.height, 0.0)
+        UIScrollView.contentInset = contentInsets
+        UIScrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification:
+        NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+    UIScrollView.contentInset = contentInsets
+    UIScrollView.scrollIndicatorInsets = contentInsets
+    }
+  
 
+    
 }
+
 
